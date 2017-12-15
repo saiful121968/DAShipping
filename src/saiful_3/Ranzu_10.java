be8213e2eedf0f7d01fd2dd57a14b3186e749c16
saiful <saiful121968@gmail.com>
2017-12-14 20:47:17 -0500

    Class Ranzu_9 and Ranzu_10 is added to my project

---
+++ src/saiful_3/Ranzu_10.java
@@ -0,0 +1,21 @@
+package saiful_3;
+
+public class Ranzu_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=30, y=45;
+		
+		boolean z =true, false;
+		if (x>y);
+		
+		System.out.println("x is greater than y =" +z);
+		
+		if (y>x);
+		System.out.println("y is greater than x"+z);
+		}
+
+
+		}
+
+
